c662837806d44a8b5b2b7741220b4123ec75bed0
gliechty <guy.liechty@gmail.com>
2017-01-27 12:25:11 -0700

    map centered on denver in test index file, markers have animation and are draggable

--- config/env.js
+++ config/env.js
@@ -1,14 +1,9 @@
-var facebookKey = {
-	key: "example key"
-}
-
 var googleKey = {
-	key: "another example"
+	key: "AIzaSyDNpsXKkrBCO49SGbKNyIRyJxBd3NbGFiI"
 }
 
 var appSecret = {
 	secret: '066064c85518ca83fec1099a15f39484'
 }
 
-module.exports = facebookKey;
 module.exports = googleKey;
